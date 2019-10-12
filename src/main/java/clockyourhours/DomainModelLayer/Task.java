884703f4fdfed0836767e965b67ff8f46c0ec690
Joost <joosttheomen@gmail.com>
2019-10-12 23:26:54 +0200

    Beetje geknutseld met client project en main om te kijken of ik de arrays kon maken is nog niet gelukt

--- src/main/java/clockyourhours/DomainModelLayer/Task.java
+++ src/main/java/clockyourhours/DomainModelLayer/Task.java
@@ -1,20 +1,12 @@
 package clockyourhours.DomainModelLayer;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
-
 public class Task {
 
-	Collection<Project> project;
-	Collection<Task_Project> task_Project;
 	private int projectID;
 	private String name;
 	private Integer costPerHour;
 
-	public Task(String name, List project)  {
+	public Task(String name)  {
 			this.name = name;
 	}
 
