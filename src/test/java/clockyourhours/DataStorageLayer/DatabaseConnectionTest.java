c9f9032d1be1eb0cc2b151d82fc1ff7c76bec34a
Joost <joosttheomen@gmail.com>
2019-10-17 22:44:53 +0200

    UNIT test voor DatabaseConnectie

---
+++ src/test/java/clockyourhours/DataStorageLayer/DatabaseConnectionTest.java
@@ -0,0 +1,44 @@
+package clockyourhours.DataStorageLayer;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseConnectionTest {
+
+//    @Test
+//    void connectionIsOpen() {
+//    }
+//
+//    @Test
+//    void closeConnection() {
+//    }
+//
+//    @Test
+//    void executeSQLSelectStatement() {
+//    }
+//
+//    @Test
+//    void executeSqlDmlStatement() {
+//    }
+
+    @Test
+    @DisplayName("Try whether connection with database can be made")
+    public void doConnectTest() {
+        // Arrange and Act
+        ResultSet rs = null;
+        DatabaseConnection connection = new DatabaseConnection();
+        boolean result = connection.openConnection();
+        if (result) {
+            rs = connection.executeSQLSelectStatement("select * from client");
+        }
+
+        // Assert
+        assertTrue("database connection successfully established", result);
+        assertTrue("result set no null", rs != null);
+    }
+}
