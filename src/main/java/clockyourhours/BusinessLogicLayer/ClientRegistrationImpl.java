6407d54a5dfc261f62528baea6ab395c72247f4b
Joost <joosttheomen@gmail.com>
2019-10-19 16:08:52 +0200

    Project bij client 2 wegschrijven naar DB

--- src/main/java/clockyourhours/BusinessLogicLayer/ClientRegistrationImpl.java
+++ src/main/java/clockyourhours/BusinessLogicLayer/ClientRegistrationImpl.java
@@ -1,9 +1,13 @@
 package clockyourhours.BusinessLogicLayer;
 
+import clockyourhours.DataStorageLayer.ClientRegistrationDAO;
+
 public class ClientRegistrationImpl implements RegistrationManager {
 
-    @Override
-    public void add() {
+
+    public static void add(String name, int IDClient) {
+        String query = "INSERT INTO Project (ProjectName, ClientID) " + "VALUES ('"+name+"','"+IDClient+"')";
+        ClientRegistrationDAO.addProjectToDatabase(query);
+    }
 
     }
-}
--- src/main/java/clockyourhours/DataStorageLayer/ClientRegistrationDAO.java
+++ src/main/java/clockyourhours/DataStorageLayer/ClientRegistrationDAO.java
@@ -1,6 +1,20 @@
 package clockyourhours.DataStorageLayer;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ClientRegistrationDAO {
 
+    public static void addProjectToDatabase(String query) {
+        try {
+            DatabaseConnection connection = new DatabaseConnection();
+            connection.openConnection();
+            connection.executeSqlDmlStatement(query);
+            connection.closeConnection();
+        } catch (Exception e) {
+            System.out.println("oeps");
+            System.out.println(e.getMessage());
 
+        }
+    }
 }
